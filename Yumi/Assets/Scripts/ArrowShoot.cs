fb8335026957b21eb2761972d62e1e18d99c6ec5
Tasos0866 <tasostsouk.tt@gmail.com>
2015-05-14 19:35:44 +0200

    initialization

---
+++ Yumi/Assets/Scripts/ArrowShoot.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowShoot : MonoBehaviour {
+	
+
+
+	private Vector3 mousePos;
+
+	//arrow properties
+	public Rigidbody arrowPrefab; 
+
+	public float arrowSpeed = 100f;
+	public float fireRate = 0.0f;
+	public float nextFire = 0.0f;
+
+	private bool falsePull;
+	private bool isPulled;
+
+	public float pullStartTime = 0.0f;
+	public float pullTime = 0f;
+	//public float maxStrengthPullTime = 1f; // how long to hold button until max strength reached
+
+	
+
+	void Start(){
+		falsePull = false;
+		isPulled = false;
+	}
+	
+	void Update(){
+
+		// pull back string
+		if(Input.GetMouseButtonDown(0))
+		{
+			mousePos = Input.mousePosition;
+			isPulled=true;
+
+
+			if(Time.time > nextFire)
+			{
+				nextFire = Time.time + fireRate; 
+				pullStartTime = Time.time; //store the start time
+				//animation.Play("PULLBACK");
+			}
+			else{
+				falsePull = true;
+			}
+
+
+		}
+
+		// fire arrow
+		if(Input.GetMouseButtonUp(0)){
+			if(!falsePull)
+			{
+				Vector3 offset = mousePos - Input.mousePosition;
+
+				/*
+				//nextFire = Time.time + pullTime; // this is the actual fire rate as things stand now
+				//animation.Play("FIRE");
+
+				float timePulledBack = Time.time - pullStartTime; // this is how long the button was held
+				if(timePulledBack > maxStrengthPullTime) // this says max strength is reached 
+					timePulledBack = maxStrengthPullTime; // max strength is ArrowSpeed * maxStrengthPullTime
+				arrowSpeed = arrowSpeed * timePulledBack; // adjust speed directly using pullback 
+				*/
+				if ( offset.magnitude > Vector3.Magnitude(new Vector3(2,2,0) )){ //only if user pulls hard enough
+					
+					Rigidbody arrowInstance = Instantiate(arrowPrefab, 			//the object
+					                                      transform.position,	//the 3d pos
+					                                      transform.rotation) 	//the rotation
+						as Rigidbody;		//reference conversions to rigidbody
+					//arrowPrefab = Rigidbody.Instantiate(arrowPrefab); //can also instantiate like this.
+					arrowInstance.name = "Arrow";
+
+				float timePulledBack = Time.time - pullStartTime; // this is how long the button was held
+				if(timePulledBack > 0.4f){ // this says max strength is reached 
+					arrowSpeed *= 1.5f; // adjust speed directly using pullback
+				}
+				if(timePulledBack>0.8f){
+					arrowSpeed *= 3f; // adjust speed directly using pullback
+				}
+				
+				arrowInstance.AddForce(offset.normalized * arrowSpeed);
+				DestroyObject(arrowInstance.gameObject, 5);
+				arrowSpeed=300;
+
+					Debug.Log (arrowInstance.mass*arrowSpeed);
+				}
+			}
+			else{
+				falsePull = false;
+			}
+		}
+
+		if (isPulled){
+			//drawTrajectory();
+		}
+	}
+
+
+
+	void drawTrajectory(){
+		/*
+		float now = Time.time;
+		float timePulledBack = Time.time - pullStartTime;
+		
+		if(timePulledBack > maxStrengthPullTime){
+			timePulledBack = maxStrengthPullTime;
+		}
+		arrowSpeed *= timePulledBack;
+		
+		if (now+1 > Time.time){
+			now=Time.time;
+			Rigidbody arrowInstance = Instantiate(arrowPrefab, 			//the object
+			                                      transform.position,	//the 3d pos
+			                                      transform.rotation) 	//the rotation
+				as Rigidbody;		//reference conversions to rigidbody
+			//arrowPrefab = Rigidbody.Instantiate(arrowPrefab); //can also instantiate like this.
+			arrowInstance.name = "Arrow";
+			Vector3 offset = mousePos - Input.mousePosition;
+			arrowInstance.AddForce(offset.normalized * arrowSpeed);
+			DestroyObject(arrowInstance.gameObject, 5);
+			arrowSpeed=1000;
+		}
+		
+		if(Input.GetMouseButtonUp(0)){
+			isPulled=false;
+		}
+		*/
+	}
+}
---
+++ Yumi/Assets/Scripts/BowControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BowControl : MonoBehaviour {
+
+
+	private bool isPressed;
+	private Vector3 mousePos;
+	private LineRenderer lineRenderer;
+
+	void Start(){
+		isPressed=false;
+
+		lineRenderer=GetComponent<LineRenderer>();
+		lineRenderer.SetPosition(0,transform.position);
+		lineRenderer.SetWidth (.1f, .1f);
+		lineRenderer.SetColors (Color.cyan,Color.cyan);
+	}
+
+	void Update() {
+
+		if (Input.GetMouseButtonDown(0)){
+			mousePos = Input.mousePosition;
+
+
+			Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+			float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle+90, Vector3.forward);
+			isPressed=true;
+
+		}
+
+		if (isPressed){
+			Vector3 dir = mousePos-Input.mousePosition;
+			float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle+90, Vector3.forward);
+			Debug.DrawRay(transform.position, dir);
+
+			lineRenderer.SetPosition (1, Input.mousePosition);
+
+			/*dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+			float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle+90, Vector3.forward);*/
+
+		}
+
+		if (Input.GetMouseButtonUp(0)){
+			isPressed=false;
+		}
+
+	}
+
+}
