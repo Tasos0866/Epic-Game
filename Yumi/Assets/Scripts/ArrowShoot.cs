465e67f93e1940ed796e7fc1d9dfba2b5c3b446f
Christos Tselepidas <ctselepidas@gmail.com>
2015-05-15 14:01:03 +0200

    unity lock
    was the prob

--- Yumi/Assets/Scripts/ArrowShoot.cs
+++ Yumi/Assets/Scripts/ArrowShoot.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 
 public class ArrowShoot : MonoBehaviour {
-	
-
 
 	private Vector3 mousePos;
+	private Vector3 offset;
 
+	private LineRenderer lineRenderer;
+	
 	//arrow properties
 	public Rigidbody arrowPrefab; 
 
@@ -14,18 +15,24 @@ public class ArrowShoot : MonoBehaviour {
 	public float fireRate = 0.0f;
 	public float nextFire = 0.0f;
 
+	private float velLimit;
 	private bool falsePull;
 	private bool isPulled;
-
+	private bool trajectoryCollision;
 	public float pullStartTime = 0.0f;
 	public float pullTime = 0f;
-	//public float maxStrengthPullTime = 1f; // how long to hold button until max strength reached
 
-	
+	//public float maxStrengthPullTime = 1f; // how long to hold button until max strength reached
 
 	void Start(){
 		falsePull = false;
 		isPulled = false;
+		trajectoryCollision= false;
+		lineRenderer=GetComponent<LineRenderer>();
+
+		velLimit=100;
+		lineRenderer.SetWidth (0.3f, 0.3f);
+		lineRenderer.enabled=false;
 	}
 	
 	void Update(){
@@ -35,7 +42,7 @@ public class ArrowShoot : MonoBehaviour {
 		{
 			mousePos = Input.mousePosition;
 			isPulled=true;
-
+			lineRenderer.enabled=true;
 
 			if(Time.time > nextFire)
 			{
@@ -46,16 +53,15 @@ public class ArrowShoot : MonoBehaviour {
 			else{
 				falsePull = true;
 			}
-
-
 		}
-
 		// fire arrow
 		if(Input.GetMouseButtonUp(0)){
 			if(!falsePull)
 			{
-				Vector3 offset = mousePos - Input.mousePosition;
-
+				isPulled=false;
+				lineRenderer.enabled=false;
+				offset = mousePos - Input.mousePosition;
+				trajectoryCollision=false;
 				/*
 				//nextFire = Time.time + pullTime; // this is the actual fire rate as things stand now
 				//animation.Play("FIRE");
@@ -74,19 +80,26 @@ public class ArrowShoot : MonoBehaviour {
 					//arrowPrefab = Rigidbody.Instantiate(arrowPrefab); //can also instantiate like this.
 					arrowInstance.name = "Arrow";
 
-				float timePulledBack = Time.time - pullStartTime; // this is how long the button was held
-				if(timePulledBack > 0.4f){ // this says max strength is reached 
-					arrowSpeed *= 1.5f; // adjust speed directly using pullback
-				}
-				if(timePulledBack>0.8f){
-					arrowSpeed *= 3f; // adjust speed directly using pullback
-				}
-				
-				arrowInstance.AddForce(offset.normalized * arrowSpeed);
-				DestroyObject(arrowInstance.gameObject, 5);
-				arrowSpeed=300;
+					/*
+					float timePulledBack = Time.time - pullStartTime; // this is how long the button was held
 
-					Debug.Log (arrowInstance.mass*arrowSpeed);
+					if(timePulledBack > 0.4f){ // this says max strength is reached 
+						arrowSpeed *= 1.5f; // adjust speed directly using pullback
+					}
+					if(timePulledBack>0.8f){
+						arrowSpeed *= 3f; // adjust speed directly using pullback
+					}
+					*/
+
+					if (offset.magnitude <= 100){
+						arrowInstance.AddForce(offset * 10);
+					}
+					else{
+						arrowInstance.AddForce(offset.normalized*1000);
+					}
+						
+					DestroyObject(arrowInstance.gameObject, 5);
+					arrowSpeed=300;
 				}
 			}
 			else{
@@ -95,39 +108,51 @@ public class ArrowShoot : MonoBehaviour {
 		}
 
 		if (isPulled){
-			//drawTrajectory();
+			offset= mousePos - Input.mousePosition;
+
+			if (offset.magnitude <= 100){
+				plotTrajectory(transform.position, offset*10 / 25f, 0.01f, 0.1f);
+			}
+			else{
+				plotTrajectory(transform.position, offset.normalized*1000/25f, 0.01f, 0.1f);
+			}
+
 		}
 	}
 
+	public Vector3 plotTrajectoryAtTime (Vector3 start, Vector3 startVelocity, float time) {
+		return start + startVelocity*time + Physics.gravity*time*time*0.5f;
+	}
+	
+	public void plotTrajectory (Vector3 start, Vector3 startVelocity, float timestep, float maxTime) {
+		//init
+		Vector3 prev = start;
+		int vertCount = 0;
+		lineRenderer.SetVertexCount(vertCount);
+		int lrIndex=0;
+		//go
+		for (int i=1;;i++) {
+			float t = timestep*i;
+			if (t > maxTime) break;
+			Vector3 pos = plotTrajectoryAtTime(start, startVelocity, t);
+			if (Physics.Linecast(prev,pos)){
+				lineRenderer.material.mainTextureScale = new Vector2(20,1);
+				lineRenderer.SetVertexCount(vertCount-1);
+				break;
+			}
+			//lineRenderer.material.
+			if (offset.magnitude>=100){
+				lineRenderer.material.mainTextureScale = new Vector2(10,1);
+			} else{
+				lineRenderer.material.mainTextureScale = new Vector2(offset.magnitude/10,1);
+			}
 
-
-	void drawTrajectory(){
-		/*
-		float now = Time.time;
-		float timePulledBack = Time.time - pullStartTime;
-		
-		if(timePulledBack > maxStrengthPullTime){
-			timePulledBack = maxStrengthPullTime;
+			Debug.DrawLine(prev,pos);
+			vertCount+=1;
+			lineRenderer.SetVertexCount(vertCount);
+			lineRenderer.SetPosition(lrIndex,prev);
+			lrIndex+=1;
+			prev = pos;
 		}
-		arrowSpeed *= timePulledBack;
-		
-		if (now+1 > Time.time){
-			now=Time.time;
-			Rigidbody arrowInstance = Instantiate(arrowPrefab, 			//the object
-			                                      transform.position,	//the 3d pos
-			                                      transform.rotation) 	//the rotation
-				as Rigidbody;		//reference conversions to rigidbody
-			//arrowPrefab = Rigidbody.Instantiate(arrowPrefab); //can also instantiate like this.
-			arrowInstance.name = "Arrow";
-			Vector3 offset = mousePos - Input.mousePosition;
-			arrowInstance.AddForce(offset.normalized * arrowSpeed);
-			DestroyObject(arrowInstance.gameObject, 5);
-			arrowSpeed=1000;
-		}
-		
-		if(Input.GetMouseButtonUp(0)){
-			isPulled=false;
-		}
-		*/
 	}
 }
--- Yumi/Assets/Scripts/BowControl.cs
+++ Yumi/Assets/Scripts/BowControl.cs
@@ -6,15 +6,10 @@ public class BowControl : MonoBehaviour {
 
 	private bool isPressed;
 	private Vector3 mousePos;
-	private LineRenderer lineRenderer;
+
 
 	void Start(){
 		isPressed=false;
-
-		lineRenderer=GetComponent<LineRenderer>();
-		lineRenderer.SetPosition(0,transform.position);
-		lineRenderer.SetWidth (.1f, .1f);
-		lineRenderer.SetColors (Color.cyan,Color.cyan);
 	}
 
 	void Update() {
@@ -34,9 +29,9 @@ public class BowControl : MonoBehaviour {
 			Vector3 dir = mousePos-Input.mousePosition;
 			float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 			transform.rotation = Quaternion.AngleAxis(angle+90, Vector3.forward);
-			Debug.DrawRay(transform.position, dir);
+			//Debug.DrawRay(transform.position, dir);
 
-			lineRenderer.SetPosition (1, Input.mousePosition);
+
 
 			/*dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
 			float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
