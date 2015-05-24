4cd6633796c2cff9c40363a915dac64224db4fe4
Christos Tselepidas <ctselepidas@gmail.com>
2015-05-24 21:16:42 +0200

    implemented double jump

--- Yumi/Assets/Scripts/Player/Player.cs
+++ Yumi/Assets/Scripts/Player/Player.cs
@@ -15,11 +15,13 @@ public class Player : Entity{
 	Vector3 velocity;
 	float velocityXSmoothing;
 
+	bool doubleJump=false;
+
 	Controller controller;
 
 	void Start (){
 		controller = GetComponent<Controller>();
-
+		
 		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
 		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
 		print ("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
@@ -28,15 +30,28 @@ public class Player : Entity{
 	
 
 	void Update (){
-		if (controller.collisions.above || controller.collisions.below) {
+		if (controller.collisions.above) {
 			velocity.y = 0;
+		}
+		if (controller.collisions.below){
+			velocity.y=0;
+			doubleJump=false;
 		}
 
 		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 
+		//single jump
 		if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
 			velocity.y = jumpVelocity;
 		}
+
+		//double jump
+		if (!controller.collisions.above && !controller.collisions.below && !doubleJump && Input.GetKeyDown (KeyCode.Space)){
+			velocity.y = jumpVelocity;
+			doubleJump=true;
+		}
+
+
 
 		float speed= (Input.GetButton("Run")? runSpeed : walkSpeed);
 
